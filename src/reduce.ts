4d48faede7cdb5f720217dd9e1ccb10edb242cbf
John Wright <jrite@protonmail.com>
2023-01-16 10:34:56 +0000

    refactor: simplify reduce with a transformstream

--- src/reduce.ts
+++ src/reduce.ts
@@ -2,39 +2,14 @@
  * Reduces chunks in to one accumulator. Once the stream is cancelled,
  * the accumulator will be queued.
  */
-export function reduce<I, O>(
-  acc: O,
-  fn: (acc: O, chunk: I) => O
-): ReadableWritablePair<O, I> {
-  const abortController = new AbortController()
+export function reduce<I, O>(acc: O, fn: (acc: O, chunk: I) => O) {
+  return new TransformStream<I, O>({
+    transform(chunk) {
+      acc = fn(acc, chunk)
+    },
 
-  return {
-    writable: new WritableStream<I>({
-      write(chunk) {
-        acc = fn(acc, chunk)
-      },
-
-      close() {
-        abortController.abort('success')
-      },
-
-      abort(reason) {
-        abortController.abort(reason)
-      },
-    }),
-
-    readable: new ReadableStream<O>({
-      start(controller) {
-        if (abortController.signal.aborted) return enqueue()
-        abortController.signal.addEventListener('abort', enqueue)
-
-        function enqueue() {
-          if (abortController.signal.reason === 'success')
-            controller.enqueue(acc)
-          else controller.error(abortController.signal.reason)
-          controller.close()
-        }
-      },
-    }),
-  }
+    flush(controller) {
+      controller.enqueue(acc)
+    },
+  })
 }
