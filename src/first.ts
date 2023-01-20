fb162d9dc199a83cdcfbe0be3306a2b4d082e424
John Wright <jrite@protonmail.com>
2023-01-20 15:09:23 +0000

    fix: first might return undefined

--- src/first.ts
+++ src/first.ts
@@ -1,9 +1,11 @@
 /**
  * Takes the first item from the stream and cancels it.
  */
-export async function first<T>(readableStream: ReadableStream<T>): Promise<T> {
+export async function first<T>(
+  readableStream: ReadableStream<T>
+): Promise<T | undefined> {
   const reader = readableStream.getReader()
-  const { value } = await reader.read()
-  reader.cancel()
-  return value as T
+  const { done, value } = await reader.read()
+  if (!done) reader.cancel()
+  return value
 }
