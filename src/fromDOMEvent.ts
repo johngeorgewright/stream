ebfb7ab4b3f6d6c45d59ccfb2d759ca619d8592b
John Wright <jrite@protonmail.com>
2023-01-20 11:45:29 +0000

    feat: add domevent options

--- src/fromDOMEvent.ts
+++ src/fromDOMEvent.ts
@@ -1,24 +1,30 @@
 export function fromDOMEvent<K extends keyof HTMLElementEventMap>(
   element: HTMLElement,
-  type: K
+  type: K,
+  options?: boolean | AddEventListenerOptions
 ): ReadableStream<HTMLElementEventMap[K]>
 
 export function fromDOMEvent(
   element: HTMLElement,
-  type: string
+  type: string,
+  options?: boolean | AddEventListenerOptions
 ): ReadableStream<Event>
 
-export function fromDOMEvent(element: HTMLElement, type: string) {
+export function fromDOMEvent(
+  element: HTMLElement,
+  type: string,
+  options?: boolean | AddEventListenerOptions
+) {
   let handler: (event: Event) => void
 
   return new ReadableStream({
     start(controller) {
       handler = (event) => controller.enqueue(event)
-      element.addEventListener(type, handler)
+      element.addEventListener(type, handler, options)
     },
 
     cancel() {
-      element.removeEventListener(type, handler)
+      element.removeEventListener(type, handler, options)
     },
   })
 }
