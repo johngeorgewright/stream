4701b76437416b317720e02db48fb4100008d14a
John Wright <jrite@protonmail.com>
2023-01-20 11:46:41 +0000

    test: fork tests

--- test/ForkableStream.test.ts
+++ test/ForkableStream.test.ts
@@ -2,18 +2,31 @@ import { fromIterable } from '../src/fromIterable'
 import { ForkableStream } from '../src/ForkableStream'
 import { write } from '../src/write'
 
-test('one subscription', async () => {
-  const forkable = new ForkableStream()
-  const fn = jest.fn()
-  fromIterable([1, 2, 3, 4, 5]).pipeTo(forkable)
+let forkable: ForkableStream<number>
+let fn: jest.Mock<void, [number]>
+let readable: ReadableStream<number>
+
+beforeEach(() => {
+  forkable = new ForkableStream()
+  fn = jest.fn()
+  readable = fromIterable([1, 2, 3, 4, 5])
+})
+
+test('fork before piping', async () => {
+  const promise = forkable.fork().pipeTo(write(fn))
+  readable.pipeTo(forkable)
+  await promise
+  expect(fn).toHaveBeenCalledTimes(5)
+})
+
+test('fork after piping', async () => {
+  readable.pipeTo(forkable)
   await forkable.fork().pipeTo(write(fn))
   expect(fn).toHaveBeenCalledTimes(5)
 })
 
 test('multiple subscribers', async () => {
-  const forkable = new ForkableStream()
-  const fn = jest.fn()
-  fromIterable([1, 2, 3, 4, 5]).pipeTo(forkable)
+  readable.pipeTo(forkable)
   await Promise.all([
     forkable.fork().pipeTo(write(fn)),
     forkable.fork().pipeTo(write(fn)),
