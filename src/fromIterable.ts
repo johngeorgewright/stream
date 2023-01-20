f7c2c23b58ccaed1df76419afe414bb85b260302
John Wright <jrite@protonmail.com>
2023-01-20 11:46:16 +0000

    feat: add iterable nodelist stream

--- src/fromIterable.ts
+++ src/fromIterable.ts
@@ -1,13 +1,21 @@
 /**
  * Creates a readable stream from an iterable of values.
  */
-export function fromIterable<T>(iterable: Iterable<T> | AsyncIterable<T>) {
+export function fromIterable<T>(
+  iterable:
+    | Iterable<T>
+    | AsyncIterable<T>
+    | { length: number; [index: number]: T }
+) {
   return new ReadableStream<T>({
     async start(controller) {
       if (Symbol.iterator in iterable)
         for (const item of iterable) controller.enqueue(item)
       else if (Symbol.asyncIterator in iterable)
         for await (const item of iterable) controller.enqueue(item)
+      else
+        for (let i = 0; i < iterable.length; i++)
+          controller.enqueue(iterable[i])
       controller.close()
     },
   })
