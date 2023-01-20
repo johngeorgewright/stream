febe32994a9347f63cf49863efae44d09e325f98
John Wright <jrite@protonmail.com>
2023-01-20 11:46:47 +0000

    tesT: from nodelists

--- test/fromIterable.test.ts
+++ test/fromIterable.test.ts
@@ -18,3 +18,16 @@ test('async iterables', async () => {
     )
   ).toEqual([0, 1, 2])
 })
+
+test('node lists', async () => {
+  for (let i = 0; i < 3; i++)
+    document.documentElement.appendChild(document.createElement('p'))
+  expect(await toArray(fromIterable(document.querySelectorAll('p'))))
+    .toMatchInlineSnapshot(`
+    [
+      <p />,
+      <p />,
+      <p />,
+    ]
+  `)
+})
