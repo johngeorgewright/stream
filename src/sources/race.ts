f17372638797ccec5d400bcb786fe651655be7ae
John Wright <jrite@protonmail.com>
2023-02-03 00:24:54 +0000

    feat(race): add race tranformer

---
+++ src/sources/race.ts
@@ -0,0 +1,36 @@
+/**
+ * Returns an `ReadableStream` that mirrors the first source stream to queue an item.
+ *
+ * @group Sources
+ * @example
+ * ```
+ * ----20----40----60------
+ * --1-----2-----3---------
+ * -----------0-----0----0-
+ *
+ * race(stream1, stream2, stream3)
+ *
+ * --1-----2-----3---------
+ * ```
+ */
+export function race<T>(...streams: ReadableStream<T>[]) {
+  const readers = streams.map((stream) => stream.getReader())
+
+  return new ReadableStream<T>({
+    async pull(controller) {
+      let done: boolean
+      let value: undefined | T
+
+      try {
+        ;({ done, value } = await Promise.race(
+          readers.map((reader) => reader.read())
+        ))
+      } catch (error) {
+        return controller.error(error)
+      }
+
+      if (done) controller.close()
+      else controller.enqueue(value)
+    },
+  })
+}
