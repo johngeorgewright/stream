1107db012bc08a83032f7557847fffb30ed7ab65
John Wright <jrite@protonmail.com>
2023-02-07 08:48:21 +0000

    test(statefulsubject): more thorough tests

--- test/subjects/StatefulSubject.test.ts
+++ test/subjects/StatefulSubject.test.ts
@@ -4,16 +4,19 @@ import {
 } from '../../src/subjects/StatefulSubject'
 import { write } from '../../src/sinks/write'
 
-test('updating a stateful resource in a stream', async () => {
-  interface State {
-    authors: string[]
-  }
+interface State {
+  authors: string[]
+}
 
-  interface Actions extends StatefulSubjectBaseActions {
-    'add author': string
-  }
+interface Actions extends StatefulSubjectBaseActions {
+  'add author': string
+  nothing: null
+}
 
-  const subject = new StatefulSubject<Actions, State>({
+let subject: StatefulSubject<Actions, State>
+
+beforeEach(() => {
+  subject = new StatefulSubject<Actions, State>({
     __INIT__: () => ({ authors: [] }),
 
     'add author': (author, state) =>
@@ -23,22 +26,94 @@ test('updating a stateful resource in a stream', async () => {
             ...state,
             authors: [...state.authors, author],
           },
+
+    nothing: (_, state) => state,
   })
+})
 
+test('the __INIT__ action', async () => {
   const fn = jest.fn()
-  const promise = subject.fork().pipeTo(write(fn))
-
-  subject.dispatch('add author', 'Jane Austin')
-  subject.dispatch('add author', 'Jane Austin')
   subject.close()
-  await promise
+  await subject.fork().pipeTo(write(fn))
 
-  expect(fn).toHaveBeenCalledTimes(2)
+  expect(fn).toHaveBeenCalledTimes(1)
   expect(fn.mock.calls).toMatchInlineSnapshot(`
     [
       [
         {
           "action": "__INIT__",
+          "param": null,
+          "state": {
+            "authors": [],
+          },
+        },
+      ],
+    ]
+  `)
+})
+
+test('a reducer that changes state', async () => {
+  const fn = jest.fn()
+  subject.dispatch('add author', 'Jane Austin')
+  subject.close()
+  await subject.fork().pipeTo(write(fn))
+
+  expect(fn).toHaveBeenCalledTimes(1)
+  expect(fn.mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        {
+          "action": "add author",
+          "param": "Jane Austin",
+          "state": {
+            "authors": [
+              "Jane Austin",
+            ],
+          },
+        },
+      ],
+    ]
+  `)
+})
+
+test('a reducer that doesnt change state', async () => {
+  const fn = jest.fn()
+  subject.dispatch('nothing', null)
+  subject.close()
+  await subject.fork().pipeTo(write(fn))
+
+  expect(fn).toHaveBeenCalledTimes(1)
+  expect(fn.mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        {
+          "action": "__INIT__",
+          "param": null,
+          "state": {
+            "authors": [],
+          },
+        },
+      ],
+    ]
+  `)
+})
+
+test('multiple calls', async () => {
+  const fn = jest.fn()
+  const promise = subject.fork().pipeTo(write(fn))
+  subject.dispatch('add author', 'Jane Austin')
+  subject.dispatch('add author', 'George Orwell')
+  subject.dispatch('add author', 'Jane Austin')
+  subject.close()
+  await promise
+
+  expect(fn).toHaveBeenCalledTimes(3)
+  expect(fn.mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        {
+          "action": "__INIT__",
+          "param": null,
           "state": {
             "authors": [],
           },
@@ -47,9 +122,22 @@ test('updating a stateful resource in a stream', async () => {
       [
         {
           "action": "add author",
+          "param": "Jane Austin",
           "state": {
             "authors": [
               "Jane Austin",
+            ],
+          },
+        },
+      ],
+      [
+        {
+          "action": "add author",
+          "param": "George Orwell",
+          "state": {
+            "authors": [
+              "Jane Austin",
+              "George Orwell",
             ],
           },
         },
