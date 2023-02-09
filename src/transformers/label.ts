087211ed45f2dbedeb4e4793d2dd283ae1427a96
John Wright <jrite@protonmail.com>
2023-02-09 11:02:47 +0000

    feat(label): introduce

---
+++ src/transformers/label.ts
@@ -0,0 +1,50 @@
+import { map } from './map'
+
+/**
+ * Maps each chunk as an object with a "label" and a "value".
+ *
+ * The label can be the result of a property on the chunk.
+ *
+ * @group Transformers
+ * @example
+ * ```
+ * --one--------------------two--------------------three----------------
+ *
+ * label('length')
+ *
+ * --{label:3,value:'one'}--{label:3,value:'two'}--{label:5,value:'five'}-
+ * ```
+ */
+export function label<T, K extends keyof T>(
+  propName: K
+): TransformStream<T, { label: T[K]; value: T }>
+
+/**
+ * The label can be the result of calling a provided function.
+ *
+ * @example
+ * ```
+ * --6.1------------------4.2------------------6.3------------------
+ *
+ * label(Math.floor)
+ *
+ * --{label:6,value:6.1}--{label:4,value:4.2}--{label:6,value:6.3}--
+ * ```
+ */
+export function label<T, L>(
+  fn: (chunk: T) => L
+): TransformStream<T, { label: L; value: T }>
+
+export function label<T, X extends keyof T | ((chunk: T) => unknown)>(
+  propNameOrFn: X
+) {
+  const getKey: (chunk: T) => unknown =
+    typeof propNameOrFn === 'function'
+      ? propNameOrFn
+      : (chunk: T) => chunk[propNameOrFn as keyof T]
+
+  return map((chunk: T) => ({
+    label: getKey(chunk),
+    value: chunk,
+  }))
+}
---
+++ test/transformers/label.test.ts
@@ -0,0 +1,59 @@
+import { fromIterable, label, write } from '../../src'
+
+test('using a property', async () => {
+  const fn = jest.fn()
+  await fromIterable(['one', 'two', 'three'])
+    .pipeThrough(label('length'))
+    .pipeTo(write(fn))
+  expect(fn.mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        {
+          "label": 3,
+          "value": "one",
+        },
+      ],
+      [
+        {
+          "label": 3,
+          "value": "two",
+        },
+      ],
+      [
+        {
+          "label": 5,
+          "value": "three",
+        },
+      ],
+    ]
+  `)
+})
+
+test('using a function', async () => {
+  const fn = jest.fn()
+  await fromIterable([6.1, 4.2, 6.3])
+    .pipeThrough(label(Math.floor))
+    .pipeTo(write(fn))
+  expect(fn.mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        {
+          "label": 6,
+          "value": 6.1,
+        },
+      ],
+      [
+        {
+          "label": 4,
+          "value": 4.2,
+        },
+      ],
+      [
+        {
+          "label": 6,
+          "value": 6.3,
+        },
+      ],
+    ]
+  `)
+})
