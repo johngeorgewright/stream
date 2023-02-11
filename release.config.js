0c190a3fa05ca5f03260d28309ef22f800f5ef79
John Wright <jrite@protonmail.com>
2023-02-11 13:07:20 +0000

    ci: fix publish command

--- release.config.js
+++ release.config.js
@@ -19,7 +19,7 @@ module.exports = {
         verifyConditionsCmd: 'yarn npm whoami --publish',
         prepareCmd:
           "yarn version ${nextRelease.version} && echo 'version=${nextRelease.version}' >> $GITHUB_OUTPUT",
-        publishCmd: 'yarn npm publish --public',
+        publishCmd: 'yarn npm publish --access public',
       },
     ],
     [
