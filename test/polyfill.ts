d4edc501a7c8dc5902fe8b772cfd7650a526701d
John Wright <jrite@protonmail.com>
2023-02-12 11:30:02 +0000

    test: add missing polyfill
    AbortSignal.timeout()

--- test/polyfill.ts
+++ test/polyfill.ts
@@ -1,1 +1,10 @@
 import 'web-streams-polyfill/es2018'
+
+if (!('timeout' in AbortSignal)) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ;(AbortSignal as any).timeout = (ms: number) => {
+    const abortController = new AbortController()
+    setTimeout(() => abortController.abort(), ms)
+    return abortController.signal
+  }
+}
