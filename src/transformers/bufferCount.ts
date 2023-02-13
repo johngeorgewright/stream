3e70ae97f1e9c28e330c08d071ff3c8b482d9e42
John Wright <jrite@protonmail.com>
2023-02-13 08:55:05 +0000

    test: buffercount

---
+++ src/transformers/bufferCount.ts
@@ -0,0 +1,31 @@
+/**
+ * Groups chunks together as an array of the specified size.
+ * When required to flush, it will queue anything left over.
+ *
+ * @group Transformers
+ * @example
+ * ```
+ * --a----b------c------d------
+ *
+ * chunks(2)
+ *
+ * -------[a,b]---------[c,d]--
+ * ```
+ */
+export function bufferCount<T, L extends number>(length: L) {
+  let buffer: T[] = []
+
+  return new TransformStream<T, T[]>({
+    transform(chunk, controller) {
+      if (buffer.length === length) {
+        controller.enqueue(buffer)
+        buffer = []
+      }
+      buffer.push(chunk)
+    },
+
+    flush(controller) {
+      if (buffer.length) controller.enqueue(buffer)
+    },
+  })
+}
