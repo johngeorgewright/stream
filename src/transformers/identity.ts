c33067e3e31ca6966564aa33ba4b7bf835647cb3
John Wright <jrite@protonmail.com>
2023-02-19 12:02:42 +0000

    feat(identity): introduce

--- src/transformers/identity.ts
+++ src/transformers/identity.ts
@@ -1,3 +1,16 @@
+/**
+ * Transforms nothing.
+ *
+ * @group Transformers
+ * @example
+ * ```
+ * --a--b--c--d--
+ *
+ * identity()
+ *
+ * --a--b--c--d--
+ * ```
+ */
 export function identity<T>() {
   return new TransformStream<T, T>()
 }
