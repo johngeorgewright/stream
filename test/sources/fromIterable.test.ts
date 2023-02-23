ca28d5158b26c55bab137259ca33e5a7ebc1582e
John Wright <jrite@protonmail.com>
2023-02-23 11:25:06 +0000

    test(fromiterable): more coverage

--- test/sources/fromIterable.test.ts
+++ test/sources/fromIterable.test.ts
@@ -4,6 +4,16 @@ test('iterables', async () => {
   expect(await toArray(fromIterable([0, 1, 2]))).toEqual([0, 1, 2])
 })
 
+test('iterators', async () => {
+  let i = 0
+  const iterator: Iterator<number> = {
+    next: () =>
+      i > 2 ? { done: true, value: undefined } : { done: false, value: i++ },
+  }
+
+  expect(await toArray(fromIterable(iterator))).toEqual([0, 1, 2])
+})
+
 test('async iterables', async () => {
   expect(
     await toArray(
@@ -18,24 +28,44 @@ test('async iterables', async () => {
   ).toEqual([0, 1, 2])
 })
 
-describe('node lists', () => {
-  beforeEach(() => {
-    for (let i = 0; i < 3; i++)
-      document.documentElement.appendChild(document.createElement('p'))
-  })
+test('async iterators', async () => {
+  let i = 0
+  const iterator: AsyncIterator<number> = {
+    next: async () =>
+      i > 2 ? { done: true, value: undefined } : { done: false, value: i++ },
+  }
 
-  afterEach(() => {
-    Array.from(document.querySelectorAll('p')).forEach((p) => p.remove())
-  })
+  expect(await toArray(fromIterable(iterator))).toEqual([0, 1, 2])
+})
 
-  test('is iterable', async () => {
-    expect(await toArray(fromIterable(document.querySelectorAll('p'))))
-      .toMatchInlineSnapshot(`
-    [
-      <p />,
-      <p />,
-      <p />,
-    ]
-  `)
-  })
+test('array likes', async () => {
+  expect(
+    await toArray(
+      fromIterable({
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        length: 3,
+      })
+    )
+  ).toEqual(['zero', 'one', 'two'])
+})
+
+test('errors', async () => {
+  let aborted = false
+  await expect(
+    fromIterable({
+      next() {
+        throw new Error('Foo')
+      },
+    }).pipeTo(
+      new WritableStream({
+        abort(reason) {
+          expect(reason).toHaveProperty('message', 'Foo')
+          aborted = true
+        },
+      })
+    )
+  ).rejects.toThrow('Foo')
+  expect(aborted).toBe(true)
 })
