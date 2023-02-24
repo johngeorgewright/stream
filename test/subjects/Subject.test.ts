cd052c531bcd669d9fbba83b75408ffc5edf09eb
John Wright <jrite@protonmail.com>
2023-02-24 14:22:03 +0000

    test(subject): coverage

--- test/subjects/Subject.test.ts
+++ test/subjects/Subject.test.ts
@@ -23,3 +23,51 @@ test('ability to queue and fork from the same object', async () => {
     ]
   `)
 })
+
+test('pulling from subject', async () => {
+  const subject = new Subject<number>()
+  let i = 0
+  subject.onPull(() => {
+    if (i === 3) {
+      subject.close()
+      return undefined
+    }
+    return ++i
+  })
+  const fn = jest.fn()
+  await subject.fork().pipeTo(write(fn))
+  expect(fn.mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        1,
+      ],
+      [
+        2,
+      ],
+      [
+        3,
+      ],
+    ]
+  `)
+})
+
+test('back pressure', async () => {
+  const subject = new Subject<number>()
+  expect(subject.desiredSize).toBe(1)
+  subject.enqueue(1)
+  expect(subject.desiredSize).toBe(0)
+})
+
+test('erroring subjects', async () => {
+  let errored = false
+  const subject = new Subject<number>()
+  const promise = subject
+    .fork()
+    .pipeTo(write())
+    .catch(() => {
+      errored = true
+    })
+  subject.error(new Error('foo'))
+  await promise
+  expect(errored).toBe(true)
+})
