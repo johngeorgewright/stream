4244e422a9a729b079991cd446e3ff447b3233c4
John Wright <jrite@protonmail.com>
2023-02-24 13:22:02 +0000

    test(fromdommutations): add tests

---
+++ test/sources/fromDOMMutations.test.ts
@@ -0,0 +1,89 @@
+import { setImmediate } from 'node:timers/promises'
+import { fromDOMMutations, write } from '../../src'
+
+test('stream of DOM mutations', async () => {
+  const fn = jest.fn()
+
+  fromDOMMutations(document.body, { childList: true })
+    .pipeThrough(
+      new TransformStream({
+        transform(chunk, controller) {
+          controller.enqueue({
+            added: chunk.addedNodes,
+            removed: chunk.removedNodes,
+          })
+        },
+      })
+    )
+    .pipeTo(write(fn))
+
+  const p = document.createElement('p')
+  p.classList.add('test')
+  document.body.appendChild(p)
+
+  await setImmediate()
+
+  const div = document.createElement('div')
+  div.appendChild(p)
+
+  await setImmediate()
+
+  document.body.appendChild(div)
+
+  await setImmediate()
+
+  expect(fn.mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        {
+          "added": NodeList [
+            <p
+              class="test"
+            />,
+          ],
+          "removed": NodeList [],
+        },
+      ],
+      [
+        {
+          "added": NodeList [],
+          "removed": NodeList [
+            <p
+              class="test"
+            />,
+          ],
+        },
+      ],
+      [
+        {
+          "added": NodeList [
+            <div>
+              <p
+                class="test"
+              />
+            </div>,
+          ],
+          "removed": NodeList [],
+        },
+      ],
+    ]
+  `)
+})
+
+test('cancelling the stream will disconnect the observer', async () => {
+  const fn = jest.fn()
+
+  fromDOMMutations(document.body, { childList: true })
+    .pipeTo(write(fn), { signal: AbortSignal.abort() })
+    .catch(() => {
+      //
+    })
+
+  const p = document.createElement('p')
+  p.classList.add('test')
+  document.body.appendChild(p)
+
+  await setImmediate()
+
+  expect(fn).not.toHaveBeenCalled()
+})
