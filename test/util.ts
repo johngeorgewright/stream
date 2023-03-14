20886f8ad1d4f411cb5fd6951811f90cbee66e39
John Wright <jrite@protonmail.com>
2023-03-14 10:37:24 +0000

    test(roundrobin): predictable timers

--- test/util.ts
+++ test/util.ts
@@ -1,3 +1,5 @@
+import { setTimeout } from 'node:timers/promises'
+
 export function defer<T = void>() {
   let $resolve: (value: T | PromiseLike<T>) => void
   let $reject: (reason?: unknown) => void
@@ -14,6 +16,16 @@ export function defer<T = void>() {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     reject: $reject!,
   }
+}
+
+export function delayedStream<T>(ms: number, items: T[]) {
+  return new ReadableStream<T>({
+    async start(controller) {
+      if (!items.length) return controller.close()
+      await setTimeout(ms)
+      controller.enqueue(items.shift())
+    },
+  })
 }
 
 export type IsString<T extends string> = T
