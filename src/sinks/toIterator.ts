5c5b7f8ca566d714b5cab62ac12d49ba78010204
John Wright <jrite@protonmail.com>
2023-03-14 13:21:08 +0000

    fix(toiterator): cleanup

--- src/sinks/toIterator.ts
+++ src/sinks/toIterator.ts
@@ -1,7 +1,17 @@
 /**
+ * Options for {@link toIterator}.
+ *
+ * @group Sinks
+ */
+export interface ToIteratorOptions {
+  signal: AbortSignal
+}
+
+/**
  * Turns a `ReadableStream` in to an `AsyncIterator`.
  *
  * @group Sinks
+ * @see {@link toIterable:function}
  * @example
  * ```
  * const iterator = toIterator(stream)
@@ -12,9 +22,45 @@
  * }
  * ```
  */
-export function toIterator<T>(stream: ReadableStream<T>): AsyncIterator<T> {
-  const reader = stream.getReader()
-  return {
-    next: () => reader.read() as Promise<IteratorResult<T>>,
+export function toIterator<T>(
+  stream: ReadableStream<T>,
+  options?: ToIteratorOptions
+): AsyncIterator<T> {
+  return new StreamIterator(stream, options?.signal)
+}
+
+class StreamIterator<T> implements AsyncIterator<T> {
+  #reader: ReadableStreamDefaultReader<T>
+  #signal?: AbortSignal
+
+  constructor(stream: ReadableStream<T>, signal?: AbortSignal) {
+    this.#reader = stream.getReader()
+    this.#signal = signal
+    if (signal?.aborted) this.throw(signal.reason)
+    signal?.addEventListener('abort', () => this.throw(signal.reason))
+  }
+
+  next() {
+    return this.#signal?.aborted
+      ? Promise.reject(this.#signal.reason)
+      : (this.#reader.read() as Promise<IteratorResult<T>>)
+  }
+
+  async return(value: T): Promise<IteratorResult<T>> {
+    try {
+      await this.#reader.cancel()
+    } catch (error) {
+      // potentially already closed
+    }
+    return { done: true, value }
+  }
+
+  async throw(reason?: unknown): Promise<IteratorResult<T>> {
+    try {
+      await this.#reader.cancel(reason)
+    } catch (error) {
+      // potentially already closed
+    }
+    return { done: true, value: undefined }
   }
 }
--- test/sinks/toIterator.test.ts
+++ test/sinks/toIterator.test.ts
@@ -1,8 +1,64 @@
-import { fromCollection, toIterable } from '../../src'
+import { ControllableStream, fromCollection, toIterator } from '../../src'
 
-test('turns a stream in to an async iterator', async () => {
-  const array: number[] = []
-  for await (const item of toIterable(fromCollection([1, 2, 3, 4, 5])))
-    array.push(item)
-  expect(array).toEqual([1, 2, 3, 4, 5])
+test('iteration over a collection of values', async () => {
+  const iterator = toIterator(fromCollection([1, 2, 3, 4, 5]))
+  const values: number[] = []
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const result = await iterator.next()
+    if (result.done) break
+    values.push(result.value)
+  }
+
+  expect(values).toStrictEqual([1, 2, 3, 4, 5])
+})
+
+test('returning from the iterator will cancel the stream', async () => {
+  const controller = new ControllableStream<number>({
+    pull(controller) {
+      controller.enqueue(1)
+    },
+  })
+
+  const iterator = toIterator(controller)
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  await iterator.return!()
+
+  expect(() => controller.enqueue(2)).toThrow(
+    'The stream is not in a state that permits enqueue'
+  )
+})
+
+test('throwing an error will cancel the stream', async () => {
+  const controller = new ControllableStream<number>({
+    pull(controller) {
+      controller.enqueue(1)
+    },
+  })
+
+  const iterator = toIterator(controller)
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  expect(await iterator.throw!(new Error('foo'))).toEqual({
+    done: true,
+    value: undefined,
+  })
+
+  expect(() => controller.enqueue(2)).toThrow(
+    'The stream is not in a state that permits enqueue'
+  )
+})
+
+test('aborting will cancel the stream', async () => {
+  const controller = new ControllableStream<number>({
+    pull(controller) {
+      controller.enqueue(1)
+    },
+  })
+
+  toIterator(controller, { signal: AbortSignal.abort(new Error('foo')) })
+
+  expect(() => controller.enqueue(2)).toThrow(
+    'The stream is not in a state that permits enqueue'
+  )
 })
