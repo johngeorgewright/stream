bc372c7504295b9f532d987f47cb2dfaca5d72b9
John Wright <jrite@protonmail.com>
2023-03-14 10:28:23 +0000

    test(distinct): dont use deprecated functions

--- test/transformers/distinct.test.ts
+++ test/transformers/distinct.test.ts
@@ -1,8 +1,8 @@
-import { ControllableStream, distinct, fromIterable, write } from '../../src'
+import { ControllableStream, distinct, fromCollection, write } from '../../src'
 
 test('only emits distinct values', async () => {
   const fn = jest.fn()
-  await fromIterable([1, 1, 2, 2, 2, 1, 2, 3, 4, 3, 2, 1])
+  await fromCollection([1, 1, 2, 2, 2, 1, 2, 3, 4, 3, 2, 1])
     .pipeThrough(distinct())
     .pipeTo(write(fn))
   expect(fn.mock.calls).toMatchInlineSnapshot(`
@@ -25,7 +25,7 @@ test('only emits distinct values', async () => {
 
 test('selecting a distinct key', async () => {
   const fn = jest.fn()
-  await fromIterable([
+  await fromCollection([
     { a: 4, n: 'f' },
     { a: 7, n: 'b' },
     { a: 5, n: 'f' },
