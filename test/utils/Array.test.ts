16cdc33c8f5f7a7fd175e2c78d3807a1632cc1e8
John Wright <jrite@protonmail.com>
2023-03-14 10:33:03 +0000

    test(without): multiple values

--- test/utils/Array.test.ts
+++ test/utils/Array.test.ts
@@ -3,4 +3,5 @@ import { utils } from '../../src'
 test('without', () => {
   expect(utils.without([1, 2, 3, 4], 2)).toEqual([1, 3, 4])
   expect(utils.without([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+  expect(utils.without([1, 2, 1, 2], 1)).toEqual([2, 1, 2])
 })
