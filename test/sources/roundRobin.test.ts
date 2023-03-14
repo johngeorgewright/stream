5f7881945be7be07c907c61044e351af6de08f22
John Wright <jrite@protonmail.com>
2023-03-14 10:35:58 +0000

    test(roundrobin): predictable timers

--- test/sources/roundRobin.test.ts
+++ test/sources/roundRobin.test.ts
@@ -1,21 +1,12 @@
-import { setTimeout } from 'node:timers/promises'
 import { roundRobin, toArray, write } from '../../src'
-
-const randomlyDelayedStream = <T>(items: T[]) =>
-  new ReadableStream({
-    async pull(controller) {
-      if (!items.length) return controller.close()
-      await setTimeout(Math.random())
-      controller.enqueue(items.shift())
-    },
-  })
+import { delayedStream } from '../util'
 
 test('it pulls, in order, from one stream at a time', async () => {
   expect(
     await toArray(
       roundRobin([
-        randomlyDelayedStream([1, 2, 3]),
-        randomlyDelayedStream(['one', 'two', 'three']),
+        delayedStream(0.3, [1, 2, 3]),
+        delayedStream(0.1, ['one', 'two', 'three']),
       ])
     )
   ).toEqual([1, 'one', 2, 'two', 3, 'three'])
@@ -25,8 +16,8 @@ test('streams that close before others will be removed from the round robin', as
   expect(
     await toArray(
       roundRobin([
-        randomlyDelayedStream([1]),
-        randomlyDelayedStream(['one', 'two', 'three']),
+        delayedStream(0.3, [1]),
+        delayedStream(0.2, ['one', 'two', 'three']),
       ])
     )
   ).toEqual([1, 'one', 'two', 'three'])
