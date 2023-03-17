511dcfce5fe43c5bb687f462ca34e2dc7b9214f1
John Wright <jrite@protonmail.com>
2023-03-17 08:54:34 +0000

    docs(debounce): add state desriptions

--- src/transformers/debounce/State.ts
+++ src/transformers/debounce/State.ts
@@ -6,7 +6,16 @@
  * @see {@link debounce:function}
  */
 export type DebounceState = Readonly<{
+  /**
+   * Whether or not the current chunk was queued to the next Readable.
+   */
   queued: boolean
+  /**
+   * The current amount of milliseconds the timer will use.
+   */
   ms: number
+  /**
+   * The timer Id/handle.
+   */
   timer?: number
 }>
