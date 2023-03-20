7b9477d8ad1123d132ce1e1f74e327cfbfb20eb9
John Wright <jrite@protonmail.com>
2023-03-20 14:23:25 +0000

    test: flushables

--- test/transformers/every.test.ts
+++ test/transformers/every.test.ts
@@ -1,4 +1,11 @@
-import { every, fromCollection, toArray } from '../../src/index.js'
+import {
+  ControllableStream,
+  every,
+  fromCollection,
+  toArray,
+  write,
+} from '../../src/index.js'
+import { timeout } from '../util.js'
 
 test('when not', async () => {
   expect(
@@ -18,4 +25,34 @@ test('when true', async () => {
       )
     )
   ).toEqual([true])
+})
+
+test('flushing', async () => {
+  const fn = jest.fn<void, [boolean]>()
+  const flushes = new ControllableStream<null>()
+  await new ReadableStream<number>({
+    async start(controller) {
+      controller.enqueue(5)
+      controller.enqueue(10)
+      controller.enqueue(15)
+      controller.enqueue(20)
+      await timeout()
+      flushes.enqueue(null)
+      await timeout()
+      controller.enqueue(25)
+      controller.close()
+    },
+  })
+    .pipeThrough(every((chunk) => chunk % 5 === 0, { flushes }))
+    .pipeTo(write(fn))
+  expect(fn.mock.calls).toMatchInlineSnapshot(`
+    [
+      [
+        true,
+      ],
+      [
+        true,
+      ],
+    ]
+  `)
 })
