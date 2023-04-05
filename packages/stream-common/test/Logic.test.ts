8b1d57b01fb4484af1e686a17bb94118458523bf
John Wright <jrite@protonmail.com>
2023-04-05 13:29:31 +0100

    feat(when): curry

--- packages/stream-common/test/Logic.test.ts
+++ packages/stream-common/test/Logic.test.ts
@@ -1,15 +1,31 @@
 import { when } from '../src/Logic.js'
 
-test('when', () => {
-  const choices = {
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    _: 'default',
-  }
+describe('when', () => {
+  test('uncurried', () => {
+    const choices = {
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      _: 'default',
+    }
 
-  expect(when(choices, 1)).toBe('one')
-  expect(when(choices, 2)).toBe('two')
-  expect(when(choices, 3)).toBe('three')
-  expect(when(choices, 4)).toBe('default')
+    expect(when(choices, 1)).toBe('one')
+    expect(when(choices, 2)).toBe('two')
+    expect(when(choices, 3)).toBe('three')
+    expect(when(choices, 4)).toBe('default')
+  })
+
+  test('curried', () => {
+    const number = when({
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      _: 'default',
+    })
+
+    expect(number(1)).toBe('one')
+    expect(number(2)).toBe('two')
+    expect(number(3)).toBe('three')
+    expect(number(4)).toBe('default')
+  })
 })
