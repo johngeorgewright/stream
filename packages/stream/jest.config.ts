b6d0f344293826aed8d84abdedb1ce31924a9316
John Wright <jrite@protonmail.com>
2023-06-30 12:21:40 +0100

    test: remove uneccesary module mapping

--- packages/stream/jest.config.ts
+++ packages/stream/jest.config.ts
@@ -1,13 +1,7 @@
 import { Config } from 'jest'
 
 const config: Config = {
-  moduleNameMapper: {
-    '^(.+)\\.js$': ['$1.js', '$1.ts', '$1.cjs', '$1.cts'],
-  },
-  setupFilesAfterEnv: [
-    require.resolve('@johngw/stream-test/polyfill'),
-    require.resolve('@johngw/stream-jest'),
-  ],
+  setupFilesAfterEnv: [require.resolve('@johngw/stream-test/polyfill')],
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/test/tsconfig.json' }],
