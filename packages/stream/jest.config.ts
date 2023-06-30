a4e77d801f2f553d1973e58df4b7adc3d946295d
John Wright <jrite@protonmail.com>
2023-06-30 12:37:28 +0100

    test: fix test module mapping

--- packages/stream/jest.config.ts
+++ packages/stream/jest.config.ts
@@ -1,6 +1,9 @@
 import { Config } from 'jest'
 
 const config: Config = {
+  moduleNameMapper: {
+    '^(.+)\\.js$': ['$1.js', '$1.ts', '$1.cjs', '$1.cts'],
+  },
   setupFilesAfterEnv: [require.resolve('@johngw/stream-test/polyfill')],
   testEnvironment: 'jsdom',
   transform: {
