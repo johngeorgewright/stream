e39319e1341b411a6865320f6697b21f37159fc2
John Wright <jrite@protonmail.com>
2023-09-26 12:04:25 +0100

    feat: display timeline position on error

--- packages/stream-jest/src/index.ts
+++ packages/stream-jest/src/index.ts
@@ -1,35 +1,43 @@
-import { expectTimeline, ParsedTimelineItemValue } from '@johngw/stream-test'
+import {
+  expectTimeline as $expectTimeline,
+  ParsedTimelineItemValue,
+} from '@johngw/stream-test'
 import { expect, JestAssertionError, MatcherContext } from 'expect'
 
 export { fromTimeline } from '@johngw/stream-test'
+
+export function expectTimeline(timeline: string) {
+  return $expectTimeline(timeline, (timelineValue, chunk, timeline) => {
+    try {
+      expect(chunk).toStrictEqual(timelineValue)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      error.message = timeline.displayTimelinePosition() + '\n' + error.message
+      throw error
+    }
+  })
+}
 
 expect.extend({
   toMatchTimeline: function toMatchTimeline<T extends ParsedTimelineItemValue>(
     this: MatcherContext,
     stream: ReadableStream<T>,
     timeline: string,
-    streamPipeOptions?: StreamPipeOptions,
+    streamPipeOptions?: StreamPipeOptions
   ) {
-    return stream
-      .pipeTo(
-        expectTimeline(timeline, (timelineValue, chunk) => {
-          expect(chunk).toStrictEqual(timelineValue)
-        }),
-        streamPipeOptions,
-      )
-      .then(
-        () => ({
-          message: () =>
-            `expect ${this.utils.printExpected(
-              stream,
-            )} to match timeline ${timeline}`,
-          pass: true,
-        }),
-        (error: JestAssertionError) => ({
-          message: () => error.matcherResult?.message || error.message,
-          pass: error.matcherResult?.pass || false,
-        }),
-      )
+    return stream.pipeTo(expectTimeline(timeline), streamPipeOptions).then(
+      () => ({
+        message: () =>
+          `expect ${this.utils.printExpected(
+            stream
+          )} to match timeline ${timeline}`,
+        pass: true,
+      }),
+      (error: JestAssertionError) => ({
+        message: () => error.matcherResult?.message || error.message,
+        pass: error.matcherResult?.pass || false,
+      })
+    )
   },
 })
 
@@ -39,7 +47,7 @@ declare global {
     interface Matchers<R> {
       toMatchTimeline(
         timeline: string,
-        streamPipeOptions?: StreamPipeOptions,
+        streamPipeOptions?: StreamPipeOptions
       ): Promise<R>
     }
   }
